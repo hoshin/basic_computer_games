5985c29549e13300c28ba0c5ac4458fbffd262a8
hoshin <martin.bahier@gmail.com>
2021-02-28 16:42:13 +0100

    (game) first pass : base prompt + basic input

---
+++ games/acey_ducey/game.js
@@ -0,0 +1,82 @@
+const prompt = require('prompt');
+const cards = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ]
+
+
+let playerMoney = 100;
+let bet = 0, firatCard = -1, secondCard = -1, availableCards = [];
+
+const bootupPrompt = () => {
+    console.log('          ACEY DUCEY CARD GAME');
+    console.log('CREATIVE COMPUTING MORRISTOWN, NEW JERSEY');
+    console.log('');
+    console.log('');
+    console.log('ACEY DUCEY IS PLAYED IN THE FOLLOWING MANNER');
+    console.log('THE DEALER (COMPUTER) DEALS TWO CARDS FACE UP');
+    console.log('YOU HAVE AN OPTION TO BET OR NOT TO BET DEPENDING');
+    console.log('ON WHETHER OR NOT YOU FEEL THE CARD WILL HAVE');
+    console.log('A VALUE BETWEEN THE FIRST TWO.');
+    console.log('IF YOU DO NOT WANT TO BET, INPUT A 0');
+    console.log(`YOU NOW HAVE ${playerMoney} DOLLARS`);
+    prompt.start();
+}
+
+
+const translateCardNumberToCardName = cardNumber => {
+    switch(cardNumber){
+        case 11: return 'JACK'
+        case 12: return 'QUEEN'
+        case 13: return 'KING'
+        case 1: return 'ACE'
+        default: return `${cardNumber}`
+    }
+}
+
+const askForBet = async () => {
+    return prompt.get(['bet'])
+}
+
+const dealCards = () => {
+    availableCards = [...Object.keys(cards)]
+
+    const one = Math.random()*(availableCards.length)
+    firstCard = cards[availableCards.splice(one, 1)[0]]
+    const two = Math.random()*(availableCards.length)
+    secondCard = cards[availableCards.splice(two, 1)[0]]
+
+
+    console.log('HERE ARE YOUR NEXT TWO CARDS')
+    console.log(firstCard)
+    console.log(secondCard)
+}
+
+const dealSingleCard = () => {
+    const one = Math.floor(Math.random()*(availableCards.length))
+    return cards[availableCards.splice(one, 1)[0]]
+}
+
+const checkIfPlayerCardIsInInterval = (highCard, lowCard, playersCard) => {
+    if(playersCard < highCard && playersCard > lowCard){
+        console.log('YOU WIN1!!!')
+    } else {
+        console.log('SORRY, YOU LOSE')
+    }
+}
+
+bootupPrompt();
+dealCards();
+askForBet().then(result => {
+    const bet = Number.parse(result.bet)
+    if(bet === 0){
+        console.log('CHICKEN!!')
+    } else if(bet > playerMoney) {
+        console.log('SORRY, FRIEND BUT YOU BLEW YOUR WAD')
+    } else {
+        const playersCard = dealSingleCard()
+        if(firstCard > secondCard){
+            checkIfPlayerCardIsInInterval(firstCard, secondCard, playersCard)
+        } else {
+            checkIfPlayerCardIsInInterval(secondCard, firstCard, playersCard)
+        }
+    }
+})
+
